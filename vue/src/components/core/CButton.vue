1958a92c02890064cd58d207492c22268b252348
FerdinandoJr <juniorferdinando@gmail.com>
2022-09-21 17:48:18 -0300

    Add core componente Button

---
+++ vue/src/components/core/CButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="c1-button">   
+    <button @click="onClickHandler()" class="botao">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  emits: ['c-click'],
+  setup (_, { emit }) {
+
+    const onClickHandler = () => emit('c-click')
+
+    return {
+      onClickHandler
+    }
+  }  
+})
+</script>
+
+<style scoped>
+
+.c1-button{
+  display: flex;
+  justify-content: right;
+
+}
+
+.botao{
+  padding: 10px 20px;
+}
+
+</style>
